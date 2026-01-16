a3c15aae398c0a3dc3778bb6706ad826200e2e6b
Vinod Cherian <vinodcherian5@gmail.com>
2026-01-17 01:46:20 +0530

    Update app.py

--- backend/api_app/app.py
+++ backend/api_app/app.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Bug tracking system API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://0.0.0.0:5173"],
+    allow_origins=["https://simple-api-frontend-bice.vercel.app", "http://localhost:5173", "http://127.0.0.1:5173", "http://0.0.0.0:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
